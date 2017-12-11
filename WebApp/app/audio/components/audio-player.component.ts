aa8406110eacd59022d5b0c1fa80b35d46d14463
Божидар Димов <bojko958@abv.bg>
2017-12-11 02:31:04 +0200

    AudioPlayer: resolved width problem using jQuery :)
    I guess jQuery is not as bad as I thought

--- WebApp/app/audio/components/audio-player.component.ts
+++ WebApp/app/audio/components/audio-player.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PlayerService } from '../../infrastructure/services/player.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'ak-audio-player',
@@ -14,22 +15,16 @@ export class AudioPlayerComponent {
     private playerService: PlayerService
   ) {
     this.ticker = setInterval(this.tick.bind(this), 100);
+    const self = this;
+
+    $(document).on("click", "#progress", function (e) {
+      let relWidth = e.pageX - $(this).offset().left;
+      let totWidth = $(this).width();
+      playerService.seek(relWidth / totWidth);
+    });
   }
 
   tick() {
     this.progress = this.playerService.calculateProgress() * 100;
-  }
-
-  seek(e) {
-    debugger;
-
-    console.log(e);
-    let widthClicked = e.offsetX;
-    let w = e.target.clientX - e.clientX;
-    let tw = e.target.clientWidth;
-    // console.log("Target width:", e.target.offsetWidth);
-    // console.log("Offset:", e.offsetX);
-    console.log("Seek percentage:", w / tw);
-    this.playerService.seek(w / tw);
   }
 }
