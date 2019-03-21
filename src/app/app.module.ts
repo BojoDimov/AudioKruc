0baaafa30a7800790d4f7cb0f705041646decec8
Божидар Димов <bojko958@abv.bg>
2019-03-21 18:58:05 +0200

    setup google api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialConfigModule } from './material-config.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCoffee, faMoon, faPowerOff, faSun } from '@fortawesome/free-solid-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+
+import { GoogleApiWrapperModule } from './google-api';
 
 import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
@@ -18,8 +19,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialConfigModule,
     FontAwesomeModule,
+    GoogleApiWrapperModule,
     ComponentsModule
   ],
   providers: [],
