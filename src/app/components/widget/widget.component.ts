030d37991703096a19bd3d07330e4ac68f36b271
Божидар Димов <bojko958@abv.bg>
2019-03-28 20:13:16 +0200

    playing with styles

--- src/app/components/widget/widget.component.ts
+++ src/app/components/widget/widget.component.ts
@@ -7,11 +7,18 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class WidgetComponent implements OnInit {
   @Input()
-  open = true;
+  open = false;
+  opening = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openWidget() {
+    this.open = true;
+    this.opening = true;
+    setTimeout(() => this.opening = false, 5 * 1000);
   }
 
 }
